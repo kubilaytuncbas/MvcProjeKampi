0030df98d1eec86027d0daa2539d1cc2fbd319dc
kubilaytuncbas <kubilaytuncbas@icloud.com>
2021-05-30 20:03:52 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        ICategoryDal categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            this.categoryDal = categoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            return categoryDal.GetAll();
+        }
+        public void CategoryAddBL(Category category)
+        {
+            if (category.CategoryName=="")
+            {
+                Console.WriteLine("hatalı giriş");
+            }
+            else
+            {
+                categoryDal.Add(category);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+
+        Context c = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _object.Add(entity);
+            c.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _object.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> GetBy(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T entity)
+        {
+            c.SaveChanges();
+        }
+    }
+}
